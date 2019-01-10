a1261bf7e0898ff2400d130f56d3bf1190896be6
Pedro <jpedrodias@gmail.com>
2019-01-10 17:01:41 +0000

    Added the HCSR04 class
    Added the UltraSonic sensor HCSR04

--- sensor_manager.py
+++ sensor_manager.py
@@ -1,5 +1,4 @@
-# Sensors_MadeEasy.py
-from time import sleep, sleep_ms
+#Sensors_MadeEasy.py
 
 class Sensor_DHT22():
   def __init__(self, PinObject):
@@ -112,6 +111,37 @@ class Sensor_BUTTONS():
   def values(self):
     return self.states
 
+class HCSR04():
+  from machine import Pin, time_pulse_us
+  from time import sleep_us
+
+  def __init__(self, trigger_pin, echo_pin, echo_timeout_us=50000):
+    self.echo_timeout_us = echo_timeout_us
+    self.trigger = Pin(trigger_pin, mode=Pin.OUT, pull=None)
+    self.trigger.value(0)
+    self.echo = Pin(echo_pin, mode=Pin.IN, pull=None)
+    
+  def _send_pulse_and_wait(self):
+    self.trigger.value(0)
+    sleep_us(5)
+    self.trigger.value(1)
+    sleep_us(10)
+    self.trigger.value(0)
+    try:
+      pulse_time = time_pulse_us(self.echo, 1, self.echo_timeout_us)
+      return pulse_time
+    except OSError as ex:
+      if ex.args[0] == 110: # 110 = ETIMEDOUT
+        raise OSError('Out of range')
+      raise ex
+  def distance_mm(self):
+    pulse_time = self._send_pulse_and_wait()
+    return pulse_time * 100 // 582
+  
+  def distance_cm(self):
+    pulse_time = self._send_pulse_and_wait()
+    return (pulse_time / 2) / 29.1
+    
 if __name__ == "__main__":
     from machine import Pin
     from time import sleep
