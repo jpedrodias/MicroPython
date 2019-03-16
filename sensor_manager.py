02fdf4ca99cd43739ceed165ade4332792180e87
Pedro <jpedrodias@gmail.com>
2019-03-16 23:28:05 +0000

    Update sensor_manager.py

--- sensor_manager.py
+++ sensor_manager.py
@@ -1,107 +1,212 @@
-# FILENAME: wlan_manager.py
-import network, time, json
+# filename: sensor_manager.py
+import machine, time
 
-class WLAN_Manager():
-  def __init__(self):
-    self.file = 'wlan_manager.json'
-    self.wlan = None
-  
-  def setup(self, ssid=None, password=None, append=False):
-    self.mode(network.AP_IF)
-    self.stop()
-    self.mode(network.STA_IF)
-    self.stop()
+class Sensor_DHT22():
+  def __init__(self, pin):
+    if not isinstance(pin, int):
+      raise TypeError('pin must be integer')
     
-    if not ssid:
-      ssid = input('WLAN SSID: ')
-    if not password:
-      password = input('password: ')
-    if append:
-      f = open(self.file, 'r')
-      data = json.loads(f.read())
-      f.close()
-    else:
-      data = {'wifi': []}
-    
-    if ssid and password:
-      data['wifi'].append({'SSID': ssid, 'PASSWORD': password})
-      f = open(self.file, 'w')
-      f.write(json.dumps(data))
-      f.close()
-      del(f)
-  #End setup
-  
-  def stop(self):
-    if self.wlan:
-      self.wlan.active(False)
-      return not self.wlan.active()
-    return True
-  #End stop
-  
-  def start(self, ssid=None, password=None):
-    # if not ssid or passowrd - get settings from file
-    if not ssid and not password:
-      try:
-        f = open(self.file, 'r')
-      except:
-        f = None
-        
-      if not f:
-        raise FileNotFoundError('{} file not found! Run .setup()'.format(self.file))
-        
-      data = json.loads(f.read())
-      f.close()
-      del(f)
-    # End if no json file is found       
-    
-    self.mode(network.STA_IF)
-    if not self.wlan.active() or not self.wlan.isconnected():
-      self.wlan.active( True )
+    from dht import DHT22
+    self.sensor = DHT22(machine.Pin(pin))
+    time.sleep( 1 ) # some delay to stabilize sensor
+    self.t = None
+    self.h = None
+  def read(self):
+    self.sensor.measure()
+    self.t, self.h = self.sensor.temperature(), self.sensor.humidity()
+    self.t, self.h = round(self.t,1), round(self.h,1)
+    return [self.t, self.h]
+  @property
+  def values(self):
+    return [self.t, self.h]
+  @property
+  def values_dict(self):
+    return {'t': self.t, 'h': self.h}
+#End of Sensor_DHT22
+
+class Sensor_DHT11():
+  def __init__(self, pin):
+    if not isinstance(pin, int):
+      raise TypeError('pin must be integer')
       
-      for cfg in data['wifi']:
-        print('\nconnecting to:', cfg['SSID'], end='')
-        self.wlan.connect(cfg['SSID'], cfg['PASSWORD'])
-        for i in range(20):
-          if self.wlan.isconnected():
-            break
-          time.sleep(1)
-          print('.', end='')
-    
-    print('\nnetwork config:', self.wlan.ifconfig())
-    return self.wlan.active()
-  #End start
-  
-  def mode(self, mode=network.AP_IF):
-    self.wlan = network.WLAN(mode)
-    return True
-  #End mode
-  def check(self):
-    if self.wlan:
-      return self.wlan.active() and self.wlan.isconnected()
-    else:
-      return False
-  #End check
-#End class 
+    from dht import DHT11
+    self.sensor = DHT11(machine.Pin(pin))
+    time.sleep(1) # some delay to stabilize sensor
+    self.t = None
+    self.h = None
+  def read(self):
+    self.sensor.measure()
+    self.t, self.h = self.sensor.temperature(), self.sensor.humidity()
+    self.t, self.h = round(self.t,1), round(self.h,1)
+    return [self.t, self.h]
+  @property
+  def values(self):
+    return [self.t, self.h]
+  @property
+  def values_dict(self):
+    return {'t': self.t, 'h': self.h}
+#End of Sensor_DHT11
 
+class Sensor_BME280():
+  def __init__(self, i2c, address=0x76):
+    if not isinstance(i2c, machine.I2C):
+      raise TypeError('I2C object required.')
+      
+    from bme280 import BME280
+    self.bme = BME280(i2c=i2c,address=address)
+    self.t = None
+    self.h = None
+    self.p = None
+  def read(self):
+    self.t, self.p, self.h = self.bme.values
+    self.t = round(self.t,1)
+    self.p = round(self.p,2)
+    self.h = round(self.h,1)
+    return [self.t, self.h, self.p]
+  @property
+  def values(self):
+    return [self.t, self.h, self.p]
+  @property
+  def values_dict(self):
+    return {'t': self.t, 'h': self.h, 'p': self.p}
+#End of Sensor_BME280
+
+class Sensor_BH1750FVI():
+  #adaptation from https://github.com/catdog2/mpy_bh1750fvi_esp8266
+  def __init__(self, i2c, address=0x23):
+    if not isinstance(i2c, machine.I2C):
+      raise TypeError('I2C object required.')
+    self.i2c = i2c
+    self.address = address
+    self.lux = None
+  def read(self):
+    self.i2c.writeto(self.address, b'\x00') # make sure device is in a clean state
+    self.i2c.writeto(self.address, b'\x01') # power up
+    self.i2c.writeto(self.address, bytes([0x23])) # set measurement mode
+    time.sleep_ms(180)
+    raw = self.i2c.readfrom(self.address, 2)
+    self.i2c.writeto(self.address, b'\x00') # power down again
+    # we must divide the end result by 1.2 to get the lux
+    self.lux = ((raw[0] << 24) | (raw[1] << 16)) // 78642
+    return self.lux
+  @property
+  def values(self):
+    return [self.lux]
+  @property
+  def values_dict(self):
+    return {'lux': self.lux}
+#End of Sensor_BH1750FVI
+
+class Sensor_DS18B20():
+  def __init__(self, pin):
+    if not isinstance(pin, int):
+      raise TypeError('pin must be integer')
+    
+    from onewire import OneWire
+    from ds18x20 import DS18X20
+    ow = OneWire(machine.Pin(ds18b20_pin)) 
+    ow.scan()
+    ow.reset()
+    self.ds18b20 = DS18X20(ow)
+    self.roms = self.ds18b20.scan()
+    self.temps = [None for rom in self.roms]
+  def read(self):
+    self.ds18b20.convert_temp()
+    time.sleep_ms(750)
+    for i, rom in enumerate(self.roms):
+      t = self.ds18b20.read_temp(rom)
+      self.temps[i] = round(t, 1)
+    return self.temps
+  @property
+  def values(self):
+    return self.temps
+  @property
+  def values_dict(self):
+    temps_dict = {}
+    for i, value in enumerate(self.temps):
+      temps_dict['t{}'.format(i)] = value
+    return temps_dict
+#End of Sensor_DS18B20
+
+class Sensor_BUTTONS():
+  def __init__(self, pins):
+    if not isinstance(pins, list):
+      raise TypeError('pins must be a list of pins')
+    
+    self.buttons = []
+    for pin in pins:
+      if not isinstance(pin, int):
+        raise TypeError('pin must be a integer')
+      self.buttons.append(machine.Pin(pin, machine.Pin.OUT))
+    self.states = [button.value() for button in self.buttons]
+    self.previews_states = [value for value in self.states]
+    self.new_event = False
+  def read(self):
+    self.new_event = False
+    for i, button in enumerate(self.buttons):
+      self.previews_states[i] = self.states[i]
+      self.states[i] = button.value()
+      self.new_event = self.new_event or (self.states[i] != self.previews_states[i])
+    return self.states
+  @property
+  def values(self):
+    return self.states
+  @property
+  def values_dict(self):
+    buttons_dict = {}
+    for i in range(len(self.states)):
+      buttons_dict['b{}'.format(i)] = self.states[i]
+      buttons_dict['_b{}'.format(i)] = self.previews_states[i]
+    return buttons_dict
+#End Sensor_BUTTONS
+
+class HCSR04():
+  def __init__(self, trigger_pin, echo_pin, echo_timeout_us=500000):
+    if isinstance(trigger_pin, int) and isinstance(echo_pin, int):
+      self.trigger = machine.Pin(trigger_pin, mode=machine.Pin.OUT, pull=None)    
+      self.echo = machine.Pin(echo_pin, mode=machine.Pin.IN, pull=None)
+    else:
+      raise TypeError('trigger_pin and echo_pin must be integer')
+    self.echo_timeout_us = echo_timeout_us
+    self.trigger.value(0)
+    self.pulse_time = None
+  def _send_pulse_and_wait(self):
+    self.trigger.value(0)
+    time.sleep_us(5)
+    self.trigger.value(1)
+    time.sleep_us(10)
+    self.trigger.value(0)
+    try:
+      pulse_time = machine.time_pulse_us(self.echo, 1, self.echo_timeout_us)
+      return pulse_time
+    except OSError as ex:
+      if ex.args[0] == 110: # 110 = ETIMEDOUT
+        raise OSError('Out of range')
+      raise ex
+  def read(self):
+    self.pulse_time = self._send_pulse_and_wait()
+    return self.pulse_time
+  @property
+  def distance_mm(self):
+    if self.pulse_time:
+      return self.pulse_time * 100 // 582
+    else:
+      return None
+  @property
+  def distance_cm(self):
+    if self.pulse_time:
+      return (self.pulse_time / 2) / 29.1
+    else:
+      return None
+  @property
+  def values(self):
+    return [self.distance_cm]
+  @property
+  def values_dict(self):
+    return {'d': self.distance_cm}
+#End of HCSR04
+  
 if __name__ == '__main__':
-  
-  wlan_client = WLAN_Manager()
-  
-  #First time, use :
-  #  wlan_client.setup()
-  #  wlan_client.setup('HOME', 'password', append=False)
-  #  wlan_client.setup('WORK', 'password', append=True)
-  
-  print('Setting wireless connecting')
-  print('Changing mode wlan AP_IF:', wlan_client.mode(network.AP_IF))
-  print('Stoping wlan AP mode:',  wlan_client.stop())
-  sleep(1)
-  
-  print('Changing mode wlan STA_IF:', wlan_client.mode( network.STA_IF ))
-  print('Stoping wlan STA mode:', wlan_client.stop() )
-  sleep(1)
-  
-  print('Starting wlan in STA mode:', wlan_client.start() )
-  print('Checking wlan:', wlan_client.check())
-  sleep(1)
-  
+  print('Sensor manager')
+
+#End of file
