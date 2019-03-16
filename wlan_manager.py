5a3af7cddc4e4ed43dfbe7ca9d6419793c154970
Pedro <jpedrodias@gmail.com>
2019-03-16 23:28:37 +0000

    Update wlan_manager.py

--- wlan_manager.py
+++ wlan_manager.py
@@ -1,87 +1,107 @@
 # FILENAME: wlan_manager.py
-from network import WLAN, AP_IF, STA_IF
-from json import loads, dumps
-        
+import network, time, json
+
 class WLAN_Manager():
   def __init__(self):
+    self.file = 'wlan_manager.json'
     self.wlan = None
   
-  def setup(self, ssid=None, password=None):
-    self.mode(AP_IF)
+  def setup(self, ssid=None, password=None, append=False):
+    self.mode(network.AP_IF)
     self.stop()
-    self.mode(STA_IF)
+    self.mode(network.STA_IF)
     self.stop()
     
     if not ssid:
       ssid = input('WLAN SSID: ')
     if not password:
       password = input('password: ')
+    if append:
+      f = open(self.file, 'r')
+      data = json.loads(f.read())
+      f.close()
+    else:
+      data = {'wifi': []}
     
     if ssid and password:
-      f = open('wlan_manager.json', 'w')
-      f.write(dumps({'SSID': ssid, 'PASSWORD': password})) 
+      data['wifi'].append({'SSID': ssid, 'PASSWORD': password})
+      f = open(self.file, 'w')
+      f.write(json.dumps(data))
       f.close()
       del(f)
+  #End setup
   
   def stop(self):
     if self.wlan:
       self.wlan.active(False)
       return not self.wlan.active()
     return True
+  #End stop
   
   def start(self, ssid=None, password=None):
+    # if not ssid or passowrd - get settings from file
     if not ssid and not password:
       try:
-        f = open('wlan_manager.json', 'r')
+        f = open(self.file, 'r')
       except:
         f = None
-    
-      if f:
-        cfg = loads(f.read())
-      else:
-        print('wlan configurations not found! Using default settings!')
-        cfg = {'SSID': 'ATLANTICO', 'PASSWORD': 'oceano12'}
-        self.setup(cfg['SSID'], cfg['PASSWORD'])
+        
+      if not f:
+        raise FileNotFoundError('{} file not found! Run .setup()'.format(self.file))
+        
+      data = json.loads(f.read())
+      f.close()
       del(f)
-    #end if not ssid and password        
+    # End if no json file is found       
     
-    self.mode(STA_IF)
+    self.mode(network.STA_IF)
     if not self.wlan.active() or not self.wlan.isconnected():
       self.wlan.active( True )
-      print('connecting to:', cfg['SSID'])
-      self.wlan.connect(cfg['SSID'], cfg['PASSWORD'])
-      while not self.wlan.isconnected():
-        pass
+      
+      for cfg in data['wifi']:
+        print('\nconnecting to:', cfg['SSID'], end='')
+        self.wlan.connect(cfg['SSID'], cfg['PASSWORD'])
+        for i in range(20):
+          if self.wlan.isconnected():
+            break
+          time.sleep(1)
+          print('.', end='')
     
-    print('network config:', self.wlan.ifconfig())
+    print('\nnetwork config:', self.wlan.ifconfig())
     return self.wlan.active()
+  #End start
   
-  def mode(self, mode=AP_IF):
-    self.wlan = WLAN(mode)
+  def mode(self, mode=network.AP_IF):
+    self.wlan = network.WLAN(mode)
     return True
-  
+  #End mode
   def check(self):
     if self.wlan:
       return self.wlan.active() and self.wlan.isconnected()
     else:
       return False
+  #End check
+#End class 
 
 if __name__ == '__main__':
-  from time import sleep
-    
+  
   wlan_client = WLAN_Manager()
+  
   #First time, use :
-  #wlan_client.setup()
+  #  wlan_client.setup()
+  #  wlan_client.setup('HOME', 'password', append=False)
+  #  wlan_client.setup('WORK', 'password', append=True)
   
   print('Setting wireless connecting')
-  print('Changing mode wlan AP_IF:', wlan_client.mode(AP_IF))
-  print('Stoping wlan:',  wlan_client.stop())
+  print('Changing mode wlan AP_IF:', wlan_client.mode(network.AP_IF))
+  print('Stoping wlan AP mode:',  wlan_client.stop())
   sleep(1)
   
-  print('Changing mode wlan STA_IF:', wlan_client.mode( STA_IF ))
-  print('Stoping wlan:', wlan_client.stop() )
+  print('Changing mode wlan STA_IF:', wlan_client.mode( network.STA_IF ))
+  print('Stoping wlan STA mode:', wlan_client.stop() )
   sleep(1)
-    
+  
   print('Starting wlan in STA mode:', wlan_client.start() )
   print('Checking wlan:', wlan_client.check())
   sleep(1)
+  
