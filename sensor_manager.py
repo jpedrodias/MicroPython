4659c7d3f082abb273408fd221364236e8743bbb
Pedro <jpedrodias@gmail.com>
2019-03-17 16:15:26 +0000

    Update sensor_manager.py

--- sensor_manager.py
+++ sensor_manager.py
@@ -1,4 +1,6 @@
 # filename: sensor_manager.py
+# WEMOS D1 Mini Board GPIO Map: D8 pull_down, D4 pull_down
+# D0=16, D1=5, D2=4, D3=0, D4=2, D5=14, D6=12, D7=13, D8=15
 import machine, time
 
 class Sensor_DHT22():
@@ -160,13 +162,14 @@ class Sensor_BUTTONS():
     return buttons_dict
 #End Sensor_BUTTONS
 
+
 class HCSR04():
-  def __init__(self, trigger_pin, echo_pin, echo_timeout_us=500000):
-    if isinstance(trigger_pin, int) and isinstance(echo_pin, int):
-      self.trigger = machine.Pin(trigger_pin, mode=machine.Pin.OUT, pull=None)    
-      self.echo = machine.Pin(echo_pin, mode=machine.Pin.IN, pull=None)
+  def __init__(self, trigger, echo, echo_timeout_us=500000):
+    if isinstance(trigger, int) and isinstance(echo, int):
+      self.trigger = machine.Pin(trigger, mode=machine.Pin.OUT, pull=None)    
+      self.echo = machine.Pin(echo, mode=machine.Pin.IN, pull=None)
     else:
-      raise TypeError('trigger_pin and echo_pin must be integer')
+      raise TypeError('trigger and echo must be integer')
     self.echo_timeout_us = echo_timeout_us
     self.trigger.value(0)
     self.pulse_time = None
@@ -209,4 +212,5 @@ class HCSR04():
 if __name__ == '__main__':
   print('Sensor manager')
 
+  
 #End of file
