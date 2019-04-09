a7b1ac021d70fbc136c8137a64cb6393cbb59de1
Pedro <jpedrodias@gmail.com>
2019-04-09 20:05:43 +0100

    Update uPhotoGate.py

--- examples/uPhotoGate.py
+++ examples/uPhotoGate.py
@@ -1,7 +1,9 @@
 # WEMOS D1 Mini Board GPIO Map: D8 pull_down, D4 pull_down
 # D0=16, D1=5, D2=4, D3=0, D4=2, D5=14, D6=12, D7=13, D8=15
-import os, gc, micropython, machine, random, time
+import os, gc, micropython, machine, time
 
+GATE_PIN = micropython.const(13) # D7
+GATE_MODE = micropython.const(0) # 0 for always on | 1 for always off
 DEBUG = micropython.const(1) # Change from 1 debug mode to 0 production mode
 DEBUG_TIME = micropython.const(10) # Run in debug mode for this amount of seconds
 DELAY_TIME = micropython.const(1)  # Delay between loops
@@ -39,13 +41,14 @@ class PhotoGate():
 #End class PhotoGate
 
 
-g1 = PhotoGate(14, mode=1) # mode = 1 | 0 
+g1 = PhotoGate(GATE_PIN, mode=0) # mode = 1 | 0 
 while True:
   g1.read()
   if g1.event_change_to(1):
     g1.start_time()
   if g1.event_change_to(0):
     g1.stop_time()
+    
     print(g1.show_ms(), 'ms')
   g1.store()
   time.sleep_us(DELAY_TIME)
