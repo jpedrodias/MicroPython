08e7c444088a29138b0415b5096360e94e080f99
Pedro <jpedrodias@gmail.com>
2019-04-20 18:02:57 +0100

    Update uPhotoGate_MQTT.py

--- examples/uPhotoGate_MQTT.py
+++ examples/uPhotoGate_MQTT.py
@@ -1,3 +1,4 @@
+# filename: main_mqtt.py
 # WEMOS D1 Mini Board GPIO Map: D8 pull_down, D4 pull_down
 # D0=16, D1=5, D2=4, D3=0, D4=2, D5=14, D6=12, D7=13, D8=15
 import os, gc, micropython, machine, time, json
@@ -11,7 +12,8 @@ GATE_MODE = micropython.const(0) # 0 for always on | 1 for always off
 DEBUG = micropython.const(1) # Change from 1 debug mode to 0 production mode
 DEBUG_TIME = micropython.const(10) # Run in debug mode for this amount of seconds
 DELAY_TIME = micropython.const(1)  # Delay between loops
-print('PhotoGate in MicroPython - v0.1')
+
+print('PhotoGate in MicroPython')
 
 from wlan_manager import WLAN_Manager # Wireless Connection
 wlan_client = WLAN_Manager()
@@ -28,24 +30,20 @@ from sensor_manager import PhotoGate
 g1 = PhotoGate(GATE_PIN, mode=GATE_MODE) # mode = 1 | 0
 
 gc.collect()
-def main():
-  while True:
-    g1.read()
-    if g1.event_change_to(1):
-      g1.start_time()
-    if g1.event_change_to(0):
-      g1.stop_time()
-      #msg = {'value': g1.show_ms(), 'units': 'ms'}
-      #print(msg['value'],'ms')
-      
-      msg = g1.show_ms()
-      print(msg,'ms')
-      mqtt_client.send(TOPIC_PUB, json.dumps(msg))
-      #gc.collect()
-    g1.store()
+while True:
+  g1.read()
+  if g1.event_change_to(1):
+    g1.start_time()
+  if g1.event_change_to(0):
+    g1.stop_time()
+    print(g1.millis)
+    msg = {'value': g1.millis, 'units': 'ms'}
+    mqtt_client.send(TOPIC_PUB, json.dumps(msg))
+    gc.collect()
+  g1.store()
+  if DEBUG:
+    time.sleep_us(DEBUG_TIME)
+  else:
     time.sleep_us(DELAY_TIME)
 #End while loops
 
-
-#if __name__ == '__main__':
-#  main()
