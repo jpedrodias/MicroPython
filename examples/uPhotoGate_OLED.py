c2dc58ac6de4f6e5dcee04de5ffda7c0f9fa9b39
Pedro <jpedrodias@gmail.com>
2019-04-20 18:05:26 +0100

    Update uPhotoGate_OLED.py

--- examples/uPhotoGate_OLED.py
+++ examples/uPhotoGate_OLED.py
@@ -1,11 +1,7 @@
-# filename: main_mqtt_oled.py
+# filename: main_oled.py
 # WEMOS D1 Mini Board GPIO Map: D8 pull_down, D4 pull_down
 # D0=16, D1=5, D2=4, D3=0, D4=2, D5=14, D6=12, D7=13, D8=15
 import os, gc, micropython, machine, time, json
-
-# Broker
-# https://www.hivemq.com/public-mqtt-broker/
-# TOPIC: devices/???/status
 
 GATE_PIN = micropython.const(13) # D7
 GATE_MODE = micropython.const(0) # 0 for always on | 1 for always off
@@ -14,17 +10,6 @@ DEBUG_TIME = micropython.const(10) # Run in debug mode for this amount of second
 DELAY_TIME = micropython.const(1)  # Delay between loops
 
 print('PhotoGate in MicroPython')
-
-from wlan_manager import WLAN_Manager # Wireless Connection
-wlan_client = WLAN_Manager()
-wlan_client.start()
-
-from mqtt_manager import MQTT_Manager # MQTT Connection
-mqtt_client = MQTT_Manager()
-mqtt_client.check() # Open connection to broker
-TOPIC_SUB = mqtt_client.get_topic('control')
-TOPIC_PUB = mqtt_client.get_topic('status')
-print('Topic:', TOPIC_PUB)
 
 # ssd1306 version
 import ssd1306
@@ -38,8 +23,7 @@ oled.fill(0)
 oled.rect(0,0,128,40,1)
 oled.text('PhotoGate', 28, 8)
 oled.text('in MicroPython', 10, 24)
-oled.text(mqtt_client.CONFIG['client_id'], 00, 48)
-oled.text(mqtt_client.CONFIG['broker'], 00, 56)
+oled.text('in milli seconds', 00, 56)
 oled.show()
 
 def update_oled(data):
@@ -57,13 +41,9 @@ while True:
     g1.stop_time()
     print(g1.millis)
     update_oled(g1.millis)
-    msg = {'value': g1.millis, 'units': 'ms'}
-    mqtt_client.send(TOPIC_PUB, json.dumps(msg))
-    gc.collect()
   g1.store()
   if DEBUG:
     time.sleep_us(DEBUG_TIME)
   else:
     time.sleep_us(DELAY_TIME)
 #End while loops
-
