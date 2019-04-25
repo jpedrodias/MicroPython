77e8636f7b5a7e271cd7e3bd1626a37ff1c01efc
Pedro <jpedrodias@gmail.com>
2019-04-25 18:33:56 +0100

    Update and rename sensor/sensor_photogate.py to sensors/sensor_photogate.py

--- sensors/sensor_photogate.py
+++ sensors/sensor_photogate.py
@@ -1,4 +1,4 @@
-# filename: sensor/uphotogate.py
+# filename: sensors/sensor_photogate.py
 # WEMOS D1 Mini Board GPIO Map: D8 pull_down, D4 pull_down
 # D0=16, D1=5, D2=4, D3=0, D4=2, D5=14, D6=12, D7=13, D8=15
 import machine, time
