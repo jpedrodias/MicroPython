5bf6b0546b4998363c761858efeb9f9ef13f2de9
Pedro <jpedrodias@gmail.com>
2019-05-01 16:10:54 +0100

    Map to GPIO on ESP8266

---
+++ board.py
@@ -0,0 +1,15 @@
+import micropython
+
+# WEMOS D1 Mini Board GPIO Map: D8 pull_down, D4 pull_down
+# D0=16, D1=5, D2=4, D3=0, D4=2, D5=14, D6=12, D7=13, D8=15
+
+D0 = micropython.const(16)
+D1 = micropython.const(5)
+D2 = micropython.const(4)
+D3 = micropython.const(0)
+D4 = micropython.const(2)
+D5 = micropython.const(14)
+D6 = micropython.const(12)
+D7 = micropython.const(13)
+D8 = micropython.const(15)
+
