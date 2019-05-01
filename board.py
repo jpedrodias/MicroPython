32bceb89d7bc984eeeb20a73b7db188734c7aeef
Pedro <jpedrodias@gmail.com>
2019-05-01 16:11:27 +0100

    Update board.py

--- board.py
+++ board.py
@@ -1,8 +1,5 @@
 import micropython
 
-# WEMOS D1 Mini Board GPIO Map: D8 pull_down, D4 pull_down
-# D0=16, D1=5, D2=4, D3=0, D4=2, D5=14, D6=12, D7=13, D8=15
-
 D0 = micropython.const(16)
 D1 = micropython.const(5)
 D2 = micropython.const(4)
