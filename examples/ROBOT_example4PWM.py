378dcef1a8982692c62d03b0d8511eb2fa4801d2
Pedro <jpedrodias@gmail.com>
2019-05-25 19:55:12 +0100

    Update ROBOT_example4PWM.py

--- examples/ROBOT_example4PWM.py
+++ examples/ROBOT_example4PWM.py
@@ -1,47 +1,52 @@
 import os, gc, micropython, machine, random, time
-from board_manager import D1, D2, D8, D7, D6, D5
-
+from board_manager import *
 from sensor_manager import Sensor_HCSR04
-sensor = Sensor_HCSR04(trigger=D1, echo=D2)
-
 from robot_manager import MotorPWM, Robot2WD
+
+sensor = Sensor_HCSR04(trigger=D3, echo=D4)
 motor1 = MotorPWM(D7, D8)
 motor2 = MotorPWM(D6, D5)
 robot = Robot2WD(motor1, motor2)
 
-motor1.speed(25)
-motor2.speed(25)
+motor1.speed(20)
+motor2.speed(22)
 
-actions2stop = ((robot.stop, 1),(robot.backward, 0.25),(robot.stop, 1))
-actions2avoid = ((robot.stop, 1),(robot.backward, 0.25),(robot.turn, 1),(robot.stop, 1))
-actions2move = ((robot.forward, 0.1),(robot.forward, 0.1),)
-actions_list = (actions2move, actions2stop, actions2avoid)
+def random_turn():
+  robot.turn(random.getrandbits(3))
 
-ACTION2MOVE = micropython.const(0)
-ACTION2STOP = micropython.const(1)
-ACTION2AVOID = micropython.const(2)
+robot_move = ((robot.forward, 0.1),)
+robot_stop = ((robot.stop, 1),)
+robot_avoid = ((robot.stop, 1),(robot.backward, 0.25),(random_turn, 0.5),(robot.stop, 1))
+robot_actions = (robot_move, robot_stop, robot_avoid)
 
-next_action = ACTION2MOVE
-last_action = ACTION2STOP
+ROBOT_MOVE = micropython.const(0) # referes to robot_actions index
+ROBOT_STOP = micropython.const(1)
+ROBOT_AVOID = micropython.const(2)
+
+next_action = ROBOT_MOVE
+last_action = ROBOT_STOP
+time.sleep(5) # time to Ctrl+C into REPL
 
 try:
   while 1:
     sensor.read()
     d = sensor.values[0]
     if d < 5:
-      next_action = ACTION2STOP
+      next_action = ROBOT_STOP
     elif d > 5 and d < 10:
-      next_action = ACTION2AVOID
+      next_action = ROBOT_AVOID
     else:
-      next_action = ACTION2MOVE
+      next_action = ROBOT_MOVE
     #End if distance
     
     if last_action != next_action:
       print('ACTION', next_action)
-      for do_action, pause in actions_list[next_action]:
+      for do_action, pause in robot_actions[next_action]:
         do_action()
         time.sleep(pause)
     #End if repeat action
+    time.sleep_ms(100)
     last_action = next_action
 except:
   robot.stop()
+  
