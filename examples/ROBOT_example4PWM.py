05fa0cf44e8a3f13e6e729079deb65407d93be55
Pedro <jpedrodias@gmail.com>
2019-05-25 20:34:50 +0100

    Update ROBOT_example4PWM.py

--- examples/ROBOT_example4PWM.py
+++ examples/ROBOT_example4PWM.py
@@ -3,19 +3,19 @@ from board_manager import *
 from sensor_manager import Sensor_HCSR04
 from robot_manager import MotorPWM, Robot2WD
 
-sensor = Sensor_HCSR04(trigger=D3, echo=D4)
-motor1 = MotorPWM(D7, D8)
-motor2 = MotorPWM(D6, D5)
-robot = Robot2WD(motor1, motor2)
+sensor = Sensor_HCSR04(trigger=D1, echo=D2)
+motorR = MotorPWM(D7, D8)
+motorL = MotorPWM(D6, D5)
+robot = Robot2WD(motorR, motorL)
 
-motor1.speed(20)
-motor2.speed(22)
+motorR.speed(42)
+motorL.speed(40)
 
 def random_turn():
   robot.turn(random.getrandbits(3))
 
 robot_move = ((robot.forward, 0.1),)
-robot_stop = ((robot.stop, 1),)
+robot_stop = ((robot.stop, 1),(robot.backward, 0.25), (robot.stop, 0.1))
 robot_avoid = ((robot.stop, 1),(robot.backward, 0.25),(random_turn, 0.5),(robot.stop, 1))
 robot_actions = (robot_move, robot_stop, robot_avoid)
 
@@ -45,8 +45,7 @@ try:
         do_action()
         time.sleep(pause)
     #End if repeat action
-    time.sleep_ms(100)
+    time.sleep_ms(250)
     last_action = next_action
 except:
   robot.stop()
-  
