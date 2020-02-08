563e85bd6f87e8bb682d09f27dc8ba63c32537e7
Pedro <jpedrodias@gmail.com>
2020-02-08 16:02:51 +0000

    Update MQTT_example_05BME280_and_OLED.py

--- examples/MQTT_example_05BME280_and_OLED.py
+++ examples/MQTT_example_05BME280_and_OLED.py
@@ -1,27 +1,28 @@
 from machine import Pin, I2C
 from utime import sleep_ms, ticks_ms, ticks_diff
 from ujson import dumps
+from ucollections import OrderedDict
 
 from wlan_manager import WLAN_Manager
 from mqtt_manager import MQTT_Manager
 
-from board_manager import  D1, D2
 from sensor_manager import Sensor_BME280
+from board_manager import  D1, D2
+
 from ssd1306 import SSD1306_I2C
 
 i2c = I2C(scl=Pin(D1), sda=Pin(D2))
 
 sensor = Sensor_BME280(i2c=i2c, address=118) # i2c.scan()
 oled = SSD1306_I2C(128, 64, i2c, 0x3c)
+oled.text("Loading ...", 0, 0)
+oled.show()
 
 wlan_client = WLAN_Manager()
 mqtt_client = MQTT_Manager()
 
-#wlan_client.setup('ATLANTICO', 'oceano12', append=False)
-#wlan_client.start()
-
-def reconnect():
-  wlan_client.start()
+def reconnect(attempts=0):
+  wlan_client.start(attempts=attempts)
   success = wlan_client.check() and mqtt_client.check()
   if success:
     mqtt_client.broker.subscribe(TOPIC_SUB)
@@ -38,40 +39,37 @@ mqtt_client.broker.set_callback(mqtt_callback)
 print( "client_id:", mqtt_client.CONFIG["client_id"] )
 
 
-pub_str = ["ColegioAtlantico", "*Clube Robotica*"]
-pub_idx = 0
-pub_counts = 10
-pub_i = 0
-
-
-DELAY = 1000 
+DELAY = mqtt_client.CONFIG["delay"] * 1000 
 while True:
   t_start = ticks_ms()
   
   sensor.read()
-  if not mqtt_client.check():
-    reconnect()
+  t = sensor.values_dict['t']
   
-  print( sensor.values_dict )
-  mqtt_client.send(TOPIC_PUB, dumps( sensor.values_dict ))
+  data = OrderedDict( [ ('t0', t0) ] )
+  print( dumps(data) )
+  
+  is_online = mqtt_client.check()
+  if not is_online:
+    is_online = reconnect()
+  
+  if is_online:
+    mqtt_client.send( TOPIC_PUB, dumps(data) )
   
   oled.fill(0)
-  oled.text("*Clube Robotica*" , 2, 4)
+  oled.text("*Clube Robotica*" , 1, 4)
   oled.rect(0,14,128,38,1)
-  oled.text("T: {} C".format( sensor.t ) , 8, 20)
-  oled.text("H: {} %".format( sensor.h ) , 8, 30)
-  oled.text("P: {} hPa".format( sensor.p ) , 8, 40)
-  
-  if pub_i == 0:
-    pub_idx = (pub_idx + 1 ) % len(pub_str)
-  pub_i = (pub_i + 1 ) % pub_counts
-  
-  oled.text( pub_str[pub_idx] , 0, 56)
+  oled.text("T0: {} C".format( t0 ) , 20, 20)
+  oled.text("T1: {} C".format( t1 ) , 20, 30)
+  oled.text("T2: {} C".format( t2 ) , 20, 40)
   oled.show()
   
-  # PAUSA ENTRE LOOPS
+  if gc.mem_free() < 10000:
+    gc.collect()
+    
+  # PAUSE
   while ticks_diff(ticks_ms(), t_start) <= DELAY:
-    mqtt_client.check_msg() # check for new messages
+    if is_online: mqtt_client.check_msg()
     sleep_ms(100)
 
 #End while True
