c05cea637aaa3fcaf214a448bc238aa4db35dbbc
Pedro Dias <jpedrodias@gmail.com>
2021-10-26 23:49:17 +0100

    Update board_manager.py

--- board_manager.py
+++ board_manager.py
@@ -1,6 +1,5 @@
 from micropython import const
 
-
 # WEMOS D1 Mini Board GPIO Map: D8 pull_down, D4 pull_down
 # D0=16, D1=5, D2=4, D3=0, D4=2, D5=14, D6=12, D7=13, D8=15
 D0 = const(16)
@@ -13,48 +12,7 @@ D6 = const(12)
 D7 = const(13)
 D8 = const(15)
 
-
-class print_colors:
-  '''Print Colored text and background
-Colors class:reset all colors with colors.reset; two sub classes fg for foreground and bg for background; use as colors.subclass.colorname.
-i.e. colors.fg.red or colors.bg.greenalso, the generic bold, disable, underline, reverse, strike through, and invisible work with the main class i.e. colors.bold'''
-    reset='\033[0m'
-    bold='\033[01m'
-    disable='\033[02m'
-    underline='\033[04m'
-    reverse='\033[07m'
-    strikethrough='\033[09m'
-    invisible='\033[08m'
-    class fg:
-        black='\033[30m'
-        red='\033[31m'
-        green='\033[32m'
-        orange='\033[33m'
-        blue='\033[34m'
-        purple='\033[35m'
-        cyan='\033[36m'
-        lightgrey='\033[37m'
-        darkgrey='\033[90m'
-        lightred='\033[91m'
-        lightgreen='\033[92m'
-        yellow='\033[93m'
-        lightblue='\033[94m'
-        pink='\033[95m'
-        lightcyan='\033[96m'
-    class bg:
-        black='\033[40m'
-        red='\033[41m'
-        green='\033[42m'
-        orange='\033[43m'
-        blue='\033[44m'
-        purple='\033[45m'
-        cyan='\033[46m'
-        lightgrey='\033[47m'
-#End class colors
-
-
 class rgb_colors():
-    # RGB - Red, Green, Blue
     brightness = 50
     RED = (255*brightness, 0, 0)
     GREEN = (0, 255*brightness, 0)
