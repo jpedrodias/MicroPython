a0a5769275279af325297486f4975fdfd4de31cb
Pedro Dias <jpedrodias@gmail.com>
2021-12-05 09:12:03 +0000

    Update board_manager.py

--- board_manager.py
+++ board_manager.py
@@ -20,11 +20,30 @@ class rgb_colors():
     BLACK = (0,0,0)
 #End RGB_Colors
 
-class Clock():
+class NTPClock():
+  @property
+  def time(self):
+    return time.time()
+  @property
+  def str_time(self):
+    l = self.local
+    g = self.gmt
+    z = self.zone
+    d = '{:04d}-{:02d}-{:02d}'.format(*l[0:3])
+    t = '{:02d}:{:02d}:{:02d}'.format(*l[3:6])
+    return '{}T{}Z{}'.format(d, t, z)
+  @property
+  def local(self):
+    return time.localtime()
+  @property
+  def gmt(self):
+    return time.gmtime()
+  @property
+  def zone(self):
+    return self.local[4]-self.gmt[4]
   @property
   def timestamp(self):
-    t = time.localtime()
-    return '{:04d}-{:02d}-{:02d} {:02d}:{:02d}:{:02d}'.format(*t[:6])
+    return self.str_time
   @staticmethod
   def update():
     try:
@@ -32,9 +51,11 @@ class Clock():
     except:
       return False
     return True
+  def __repr__(self):
+    return self.str_time
 
 class StatusLED():
-  def __init__(self, pin):
+  def __init__(self, pin=2):
     self.led = machine.Pin(pin, machine.Pin.OUT)
   def value(self, value):
     self.led.value(value)
@@ -44,3 +65,4 @@ class StatusLED():
     self.value(0)
   def toggle(self):
     self.led.value(not self.led.value())
+    
