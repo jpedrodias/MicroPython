a44043ade30d0b4aa361af6d1ea5f227067342a4
Pedro Dias <jpedrodias@gmail.com>
2023-10-30 13:07:46 +0000

    Update banho_maria.py

--- examples/banho_maria.py
+++ examples/banho_maria.py
@@ -23,15 +23,16 @@ LIMITE_HEAT = 0.01 * DELAY
 sensor.read()
 
 temps = [sensor.values[0], sensor.values[0]]
-temps_index = 0
-status, status_last = 0, 1
+
+status, status_last = 0, 0
 
 print(f"Temp; Status; Gradiente; Heat_Limit")
 while True:
   sensor.read()
   if sensor.values:
-      temps[temps_index] = sensor.values[0]
-      gradient = (temps[1] - temps[0] ) / DELAY 
+      temps[0] = temps[1]
+      temps[1] = sensor.values[0]
+      gradient = (temps[1] - temps[0] ) / DELAY
       if temps[1] > LIMITE_HIGH:
           status = 0
       elif temps[1] < LIMITE_LOW:
@@ -43,7 +44,6 @@ while True:
   if status != status_last:
       relay1.value(status)
       
-  print(f"Temp: {temps[0]}; {status}; {round(gradient, 2)}; {LIMITE_HEAT}")
+  print(f"{temps[0]}; {status}; {round(gradient, 2)}; {LIMITE_HEAT}")
   sleep(DELAY)
   status_last = status
-  temps_index = (temps_index + 1 ) % 2
