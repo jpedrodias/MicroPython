171907e5f913f0f6a0ca3a44098b71a405ef209f
Pedro Dias <jpedrodias@gmail.com>
2024-03-12 11:37:14 +0000

    Update neopixel_newton_disk.py

--- examples/neopixel_newton_disk.py
+++ examples/neopixel_newton_disk.py
@@ -1,30 +1,32 @@
 from neopixel import NeoPixel
 from machine import Pin
 from time import sleep
-from board_manager import D7
+from board_manager import D2
 
 class Color:
     # RGB - Red, Green, Blue
-    brightness = 50 # 0 - 255
-    R = (brightness, 0, 0)
-    G = (0, brightness, 0)
-    B = (0, 0, brightness)
+    X= 50 # 0 - 255 # brightness
+    R = (X, 0, 0)
+    G = (0, X, 0)
+    B = (0, 0, X)
     BK = (0,0,0)
-    
-neo = NeoPixel(Pin(D7), 1)
+
+num_pixeis = 1
+neo = NeoPixel(Pin(D2), num_pixeis)
 neo.fill(Color.BK)
 neo.write()
 
-PAUSE = 0.5 # CHANGE THIS VALUE
+PAUSE = 1 # CHANGE THIS VALUE
 DURATION = 10 # seconds
 colors = [Color.R, Color.G, Color.B]
 #colors = [Color.R, Color.G]
 #colors = [Color.G, Color.B]
-#colors = [Color.R, Color.G]
+#colors = [Color.R, Color.B]
+
 ncolors = len(colors)
 
 for i in range(int(DURATION / PAUSE)):
-  neo.fill(colors[i%ncolors])
+  neo.fill(colors[i % ncolors])
   neo.write()
   sleep(PAUSE)
 
