210b34b29875babcc08a9df392a3fa9d46286309
Pedro Dias <jpedrodias@gmail.com>
2025-10-01 23:00:53 +0100

    Simplify WLAN setup and module installation
    Refactor initial setup script to simplify WLAN connection and remove unnecessary function. Added direct installation of required modules.

--- examples/initial_setup.py
+++ examples/initial_setup.py
@@ -1,46 +1,29 @@
-def init_setup(ssid, password):
-  import network
-  import time
-  import mip
-   
-  wlan = network.WLAN(network.STA_IF)  
-  wlan.active(True)
-  wlan.connect(ssid, password) 
-  
-  while not wlan.isconnected() and wlan.status() >= 0:
-    print("Waiting to connect:")
+import network, time, mip
+
+SSID = 'YOUR_SSID' # 2.4Gh
+PASS = 'your-password'
+
+wlan = network.WLAN(network.STA_IF)  
+wlan.active(True)
+wlan.connect(SSID, PASS) 
+
+print("Waiting to connect:")
+while not wlan.isconnected() and wlan.status() >= 0:
+    print('.', end='')
     time.sleep(1)
 
-  print(wlan.ifconfig())
-  
-  print('Downloading umqtt.simple')
-  mip.install('umqtt.simple', target='/')
-  
-  print('Downloading wlan_manager.mpy')
-  mip.install('https://github.com/jpedrodias/MicroPython/raw/master/compiled/wlan_manager.mpy', target='/')
-  
-  print('Downloading mqtt_manager.mpy')
-  mip.install('https://github.com/jpedrodias/MicroPython/raw/master/compiled/mqtt_manager.mpy', target='/')
-  
-if __name__ == "__main__":
-  import time
-  
-  SSID_NAME = 'change this'
-  SSID_PASSWORD = 'change this'
-  init_setup(SSID_NAME, SSID_PASSWORD)
-  
-  print('WLAN Setup')
-  from wlan_manager import WLAN_Manager
-  wlan_client = WLAN_Manager()
-  wlan_client.stop()
-  time.sleep(1)
-  wlan_client.setup(SSID_NAME, SSID_PASSWORD, append=False)
-  time.sleep(1)
-  wlan_client.start()
-  
-  print('MQTT Setup')
-  from mqtt_manager import MQTT_Manager
-  mqtt_client = MQTT_Manager()
-  mqtt_client.setup()
-  print(mqtt_client.get_topic('control'))
-  print(mqtt_client.get_topic('status'))
+print(wlan.ifconfig())
+
+downloads = [
+        'https://raw.githubusercontent.com/stlehmann/micropython-ssd1306/refs/heads/master/ssd1306.py',
+        'https://raw.githubusercontent.com/jpedrodias/MicroPython/refs/heads/master/compiled/sensor_manager.mpy',
+        'https://raw.githubusercontent.com/jpedrodias/MicroPython/refs/heads/master/compiled/wlan_manager.mpy',
+        'https://raw.githubusercontent.com/jpedrodias/MicroPython/refs/heads/master/compiled/mqtt_manager.mpy',
+        'https://raw.githubusercontent.com/jpedrodias/MicroPython/refs/heads/master/compiled/bme280.mpy',
+        'https://raw.githubusercontent.com/jpedrodias/MicroPython/refs/heads/master/compiled/bmp085.mpy'
+    ]
+
+for file in downloads:
+    mip.install(file)
+    
+print('All done!')
